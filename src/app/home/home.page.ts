9048b9e0b014187fbc9c5e5ade052abba6d824d4
couscoudet <ysoultane@gmail.com>
2024-09-11 12:24:12 +0200

    first commit functional counter

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { KeepAwake } from '@capacitor-community/keep-awake';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  counter: number = 0;
 
   constructor() {}
 
+  ngOnInit() {
+    this.keepScreenAwake();
+  }
+
+  incrementCounter() {
+    this.counter++;
+  }
+
+  async keepScreenAwake() {
+    await KeepAwake.keepAwake();
+  }
 }
